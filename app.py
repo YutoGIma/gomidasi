ee226d38fc1f37365d03b134c84699cd97c67c3b
Gima Yuto <yurihanagima@gmail.com>
2021-05-18 16:27:57 +0900

    add selectcity develop

---
+++ app.py
@@ -0,0 +1,13 @@
+import sqlite3
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+@app.route("/select_city")
+def select_city():
+    return render_template("select_city.html")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
