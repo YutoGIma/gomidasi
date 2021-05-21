f7fd1142b0476513bb3d6a553e1b86a33b108a5a
Gima Yuto <yurihanagima@gmail.com>
2021-05-21 15:39:37 +0900

    develop bell

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import sqlite3
-from flask import Flask,render_template,request,redirect
+from flask import Flask,render_template,request,redirect,session
 
 app=Flask(__name__)
+app.secret_key="sunabacokoza"
 
 @app.route("/select_city")
 def select_city():
@@ -51,19 +52,39 @@ def erea_sc():
     for row in c.fetchall():
         erea.append({"id":row[0],"erea":row[1]})
     c.close()
+    session["erea_id"]=erea["id"]
     return render_template("erea.html",erea=erea)
 
-@app.route("/topmenu")
-def topmenu():
-    return render_template("topmenu.html")
+@app.route("/topmenu/<int:id>")
+def topmenu(id):
+    conn=sqlite3.connect("ゴミ分別DB.db")
+    c=conn.cursor()
+    c.execute("insert into users values (null,?,null,0,0,0,0,0,0,0,0,0,0,0,0,0,0)",(id,))
+    conn.commit()
+    c.execute("select erea,cal from erea where id=? ",(id,))
+    sel_erea=c.fetchall()
+    cal=sel_erea[0][1]
+    sel_erea=sel_erea[0][0]
+    c.close()
+    return render_template("topmenu.html",sel_erea=sel_erea,cal=cal)
 
 @app.route("/bell")
 def bell():
     return render_template("bell.html")
 
+@app.route("/bell_fire_day")
+def bell_fire_day():
+    return redirect("/bell")
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+@app.route("/del_select")
+def del_select():
+    session.pop("erea_id",None)
+    return redirect("/select_city")
 
 
 
